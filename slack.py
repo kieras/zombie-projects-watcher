b511bc44a943c047d28f82b140e403e640ab52ec
Anderson Junior <andersonj@ciandt.com>
2021-11-26 16:51:11 -0300

    review (pr#59): extend the changes to slack.py

--- slack.py
+++ slack.py
@@ -21,6 +21,7 @@ BOT_NAME = CONFIG['slack']['bot']['name'].get()
 BOT_EMOJI = CONFIG['slack']['bot']['emoji'].get()
 COST_ALERT_THRESHOLD = CONFIG['slack']['cost_alert_threshold'].get(float)
 COST_ALERT_EMOJI = CONFIG['slack']['cost_alert_emoji'].get()
+COST_MIN_TO_NOTIFY = CONFIG['chat']['cost_min_to_notify'].get(float)
 
 
 def send_messages(projects_by_owner):
@@ -31,6 +32,7 @@ def send_messages(projects_by_owner):
     for owner in projects_by_owner.keys():
         slack_user = USERS_MAP.get(owner, owner)
         message = "Hey @{}, I've noticed you are one of the owners of the following projects:\n".format(slack_user)
+        send_message_to_this_owner = False
         for project in projects_by_owner.get(owner):
             project_id = project.get('projectId')
             org = ORGS_NAME_MAPPING.get(project.get('parent').get('id'))
@@ -38,23 +40,28 @@ def send_messages(projects_by_owner):
             cost = project.get('costSincePreviousMonth', 0.0)
             currency = project.get('costCurrency', '$')
             emoji = ''
-            if cost > COST_ALERT_THRESHOLD:
-                emoji = ' ' + COST_ALERT_EMOJI
-            message += "> - `{}/{}` created `{} days ago`, costing *`{}`* {}.{}\n".format(org, project_id, created_days_ago, cost, currency, emoji)
+            if cost <= COST_MIN_TO_NOTIFY:
+                logger.debug('- `{}/{}`, will not be in the message, due to its cost being lower than the minimum warning value'.format(owner, project_id))
+            else:
+                if cost > COST_ALERT_THRESHOLD:
+                    emoji = ' ' + COST_ALERT_EMOJI
+                    send_message_to_this_owner = True
+                message += "- `{}/{}` created `{} days ago`, costing *`{}`* {}.{}\n".format(org, project_id, created_days_ago, cost, currency, emoji)
         message += "If these projects are not being used anymore, please consider `deleting them to reduce infra costs` and clutter. :rip:"
-        slack_channel = "@{}".format(slack_user)
-        if SEND_TO_TEAM_CHANNEL:
-            slack_channel = "#{}".format(TEAM_CHANNEL)
-        resp = _send_message(slack_channel, message)
-        if resp and not resp.get('ok'):
-            if resp.get('error') == 'channel_not_found':
-                logger.error('Error: %s, Channel: %s', resp.get('error'), slack_channel)
-                if TEAM_CHANNEL_FALLBACK:
-                    resp = _send_message("#{}".format(TEAM_CHANNEL), message)
-                    if resp and not resp.get('ok'):
-                        logger.error('Error in fallback to tema channel: %s, Channel: %s, Response: %s', resp.get('error'), slack_channel, pformat(resp))
-                else:
-                    logger.error('Error: %s, Channel: %s, Response: %s', resp.get('error'), slack_channel, pformat(resp))
+        if send_message_to_this_owner:
+            slack_channel = "@{}".format(slack_user)
+            if SEND_TO_TEAM_CHANNEL:
+                slack_channel = "#{}".format(TEAM_CHANNEL)
+            resp = _send_message(slack_channel, message)
+            if resp and not resp.get('ok'):
+                if resp.get('error') == 'channel_not_found':
+                    logger.error('Error: %s, Channel: %s', resp.get('error'), slack_channel)
+                    if TEAM_CHANNEL_FALLBACK:
+                        resp = _send_message("#{}".format(TEAM_CHANNEL), message)
+                        if resp and not resp.get('ok'):
+                            logger.error('Error in fallback to tema channel: %s, Channel: %s, Response: %s', resp.get('error'), slack_channel, pformat(resp))
+                    else:
+                        logger.error('Error: %s, Channel: %s, Response: %s', resp.get('error'), slack_channel, pformat(resp))
 
 
 def _send_message(channel, message):
