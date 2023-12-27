00d109cfb16f931fccbb5697291531e3e1de1383
Leonardo Romanini <97033241+romanini-ciandt@users.noreply.github.com>
2023-12-27 10:46:34 -0300

    Update chat.py
    Co-authored-by: Daniel Andrade <dandrade@ciandt.com>

--- chat.py
+++ chat.py
@@ -77,7 +77,7 @@ def send_messages_to_chat(projects_by_owner):
             send_message(message)
 
     today_weekday=dt.today().strftime('%A')
-    final_of_execution_message = f'Happy {today_weekday}! \
-\nToday I found *{number_of_notified_projects} projects* with costs higher than the defined notification threshold of ${COST_MIN_TO_NOTIFY}.\
-\n\n'
+    final_of_execution_message = f'''Happy {today_weekday}!
+Today I found *{number_of_notified_projects} projects* with costs higher
+than the defined notification threshold of ${COST_MIN_TO_NOTIFY}.'''
     send_message(final_of_execution_message)
